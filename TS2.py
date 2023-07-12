49a59fad27c0717cd471c8be8d6db3938a151c18
Jamil Palacios <japalac5@outlook.com>
2023-07-12 08:09:18 -0400

    color added

--- TS2.py
+++ TS2.py
@@ -3,10 +3,10 @@ begin_graphics()
 x=1
 y=1
 #such a square
-Line((x+300,y+50),(x+25,y-50))
-Line((x+300,y-188),(x+25,y+188))
-Line((x+25,y+50),(x+25,y+188))
-Line((x+300,y-50),(x+300,y+188))
+Line((x+300,y+50),(x+25,y+50), color='#008000')
+Line((x+300,y+188),(x+25,y+188), color='#008000')
+Line((x+25,y+50),(x+25,y+188), color='#008000')
+Line((x+300,y+50),(x+300,y+188), color='#008000')
 
 #hats off
 Arc((x+290,y+170),45, 155, -75)
@@ -16,12 +16,12 @@ Line((x+330,y+190),(x+370,y+260))
 Line((x+315,y+280),(x+370,y+260))
 
 #symbolism
-Line((x+40,y+118),(x+165,y+60))
-Line((x+40,y+118),(x+165,y+178))
-Line((x+285,y+118),(x+165,y+60))
-Line((x+285,y+118),(x+165,y+178))
-Circle((x+165,y+118), 30)
-Line((x+130,y+130),(x+195,y+110))
+Line((x+40,y+118),(x+165,y+60), color='#FFFF00')
+Line((x+40,y+118),(x+165,y+178), color='#FFFF00')
+Line((x+285,y+118),(x+165,y+60), color='#FFFF00')
+Line((x+285,y+118),(x+165,y+178), color='#FFFF00')
+Circle((x+165,y+118), 30, filled=True, color='#000080')
+Line((x+130,y+130),(x+195,y+110), color='#ffffff')
 
 update_when('key_pressed')
 end_graphics
