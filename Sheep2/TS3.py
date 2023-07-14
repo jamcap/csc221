5a11999595896ea7f4387eecfee9db4fa0cffe5b
Jamil Palacios <japalac5@outlook.com>
2023-07-13 22:25:56 -0400

    comments and grammer edits

--- Sheep2/TS3.py
+++ Sheep2/TS3.py
@@ -2,27 +2,30 @@ from gasp.utils import read_number
 from gasp.utils import read_yesorno
 from random import randint
 
-def gm1 ():
-    print("great\n please enter you 6 digit(s), within the number 40")
+#quickGame
+def op1 ():
+    print("great\nplease enter you 6 digit(s), within the number 40")
     l1 = []
     for i in range(0,6):
-        k = int(input)
+        k = int(input())
         l1.append(k)
 
+#finishOne
 def stp():
-    print("then i'm afriad we have nothing left ot offer you.")
+    print("then i'm afriad we have nothing left to offer you.")
     SystemExit()
 
-def op1():
+#longGame
+def op2():
     if read_yesorno("would you prefer a long game?"):
         print("great")
     else:
         stp()
 
+#start
 print()
-print("welcome back to the VA lottery") 
-print("please make a selection")
+print("welcome back to the VA lottery\nplease make a selection")
 if read_yesorno("would you like to play a quick game?"):
-    gm1()
+    op1()
 else: 
-    op1() 
+    op2() 
