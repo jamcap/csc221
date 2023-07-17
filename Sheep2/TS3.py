38c983ac34a3eafe49903dc65cd94b13749a7f20
Jamil Palacios <japalac5@outlook.com>
2023-07-17 08:03:24 -0400

    first test of game

--- Sheep2/TS3.py
+++ Sheep2/TS3.py
@@ -1,14 +1,37 @@
 from gasp.utils import read_number
 from gasp.utils import read_yesorno
+from collections import Counter
 from random import randint
+on = True
+l1 =[]
+x = l1
+l2 =[]
+y = l2
 
 #quickGame
 def op1 ():
-    print("great\nplease enter you 6 digit(s), within the number 40")
-    l1 = []
-    for i in range(0,6):
-        k = int(input())
-        l1.append(k)
+    while on == True:
+        print("great\nplease enter you 6 numbers(s), within the number 40")
+        for i in range(0,6):
+            k = int(input())
+            l1.append(k)
+        for i in range(0,6):
+            k1 = randint(0,40)
+            l2.append(k1)
+        print("the random numbers were " + str(l2))
+        acc()
+        if read_yesorno("would you like to play agian?"):
+            print("ok")
+        else:
+            break
+
+
+def acc():
+    c1 = Counter(l1)
+    c2 = Counter(l2)
+    c3 = list((c1 & c2).elements())
+    c5 = len(c3) / len(set(l1 + l2))*100
+    print("you had an accuracy of " + str(c5) + "%")
 
 #finishOne
 def stp():
