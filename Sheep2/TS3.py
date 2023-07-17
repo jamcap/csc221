266d5f7137f227605c462719ef8cff01ba20b5c6
Jamil Palacios <japalac5@outlook.com>
2023-07-17 08:12:15 -0400

    first test of long game

--- Sheep2/TS3.py
+++ Sheep2/TS3.py
@@ -23,8 +23,26 @@ def op1 ():
         if read_yesorno("would you like to play agian?"):
             print("ok")
         else:
+            print("thanks for playing")
             break
 
+
+def gm2():
+    while on == True:
+        print("great\nplease enter you 8 numbers(s), within the number 40")
+        for i in range(0,8):
+           k = int(input())
+           l1.append(k)
+        for i in range(0,8):
+            k1 = randint(0,80)              
+            l2.append(k1)
+        print("the random numbers were " + str(l2))
+        acc()
+        if read_yesorno("would you like to play agian?"):
+            print("ok")
+        else:
+            print("thanks for playing")
+            break
 
 def acc():
     c1 = Counter(l1)
@@ -33,7 +51,7 @@ def acc():
     c5 = len(c3) / len(set(l1 + l2))*100
     print("you had an accuracy of " + str(c5) + "%")
 
-#finishOne
+#finished
 def stp():
     print("then i'm afriad we have nothing left to offer you.")
     SystemExit()
@@ -41,7 +59,7 @@ def stp():
 #longGame
 def op2():
     if read_yesorno("would you prefer a long game?"):
-        print("great")
+        gm2()
     else:
         stp()
 
