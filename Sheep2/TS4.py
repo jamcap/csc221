ea8a1c52c8a31be290c08b7ab4e07ab9528a4c96
Jamil Palacios <japalac5@outlook.com>
2023-07-19 10:24:07 -0400

    working though revisions 2

--- Sheep2/TS4.py
+++ Sheep2/TS4.py
@@ -5,15 +5,15 @@ import time
 u_x = randint(0, 63)
 u_y = randint(0, 47)
 
+
 def place_player():
-     global c
-     c = Circle((10 * u_x + 5, 10 * u_y + 5), 10, filled=True, color= "#008000")
+    c
 
 def move_player():
     global u_x, u_y
     while True:
         key = update_when('key_pressed')
-        if key == '1':
+        if key == "1":
             u_x += 1
             u_y += 0
         elif key == '2':
@@ -37,12 +37,15 @@ def move_player():
             if key == '8':
                 u_x += 1
                 u_y += 1
+        move_to(c, (Circle, (10*u_x, 10* u_y), 10,))
+
 
 def pas():
     time.sleep(5)
 
 begin_graphics()  
-c = Circle((320, 200), 10, filled=True, color="#FFFF00")
+c = Circle((10*u_x, 10*u_y),10, filled=True, color="#FFFF00")
+
 place_player()
 move_player()
 #key_text = Text("a", (320, 240), size=48)
