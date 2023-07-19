3a4a14485c7cd0a8d3fabb682e14640bfa748e3f
Jamil Palacios <japalac5@outlook.com>
2023-07-19 08:54:48 -0400

    working though revisions

--- Sheep2/TS4.py
+++ Sheep2/TS4.py
@@ -1,42 +1,50 @@
 from gasp import *
 from random import randint
+import time
 
-player_x = randint(0, 63)
-player_y = randint(0, 47)
-player_shape = Circle((300,200), 15)
+u_x = randint(0, 63)
+u_y = randint(0, 47)
 
 def place_player():
-    Circle((10 * player_x, 10 * player_y), 5, filled=True)
+     global c
+     c = Circle((10 * u_x + 5, 10 * u_y + 5), 10, filled=True, color= "#008000")
 
 def move_player():
-    global player_x, player_y, player_shape
+    global u_x, u_y
+    while True:
+        key = update_when('key_pressed')
+        if key == '1':
+            u_x += 1
+            u_y += 0
+        elif key == '2':
+            u_x += 1
+            u_y += 1
+            if key == '3':
+                u_x += 0
+                u_y += 1
+            if key == '4':
+                u_x -= 1
+                u_y += 1
+            if key == '5':
+                u_x -= 1
+                u_y += 0
+            if key == '6':
+                u_x -= 1
+                u_y -= 1
+            if key == '7':
+                u_x -= 0
+                u_y -= 1
+            if key == '8':
+                u_x += 1
+                u_y += 1
 
-    key = update_when('key_pressed')
-    print("moved")
+def pas():
+    time.sleep(5)
 
-    if key == 'k' and player_x <63:
-        player_x += 1
-    elif key == 'l':
-        if player_x < 63:
-            player_x += 1
-        if player_y > 0:
-            player_y -= 1 
-
-    move_to(player_shape, (10 * player_x + 5, 10 * player_y + 5))
-
-begin_graphics()
-finished = False
-c = Circle((300,200), 5)
-key_text = Text("", (320,240), size =48)
-
+begin_graphics()  
+c = Circle((320, 200), 10, filled=True, color="#FFFF00")
 place_player()
 move_player()
-
-while True:
-    key = update_when('key_pressed')
-    remove_from_screen(key_text)
-    key_text = Text(key, (320,240), size=48)
-    if key == 'q':
-        break
+#key_text = Text("a", (320, 240), size=48)
 
 end_graphics()
---
+++ Sheep2/TS5.py
@@ -0,0 +1,9 @@
+'Python'[1]
+#"Strings are sequences of characters."[5]
+#len("wonderful")
+#'Mystery'[:4]
+#'p' in 'Pineapple'
+#'apple' in 'Pineapple'
+#'pear' not in 'Pineapple'
+#(2, 4, 6, 8, 10)[1:4]
+#[("cheese", "queso"), ("red", "rojo")][1][0][2]
