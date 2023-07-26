0af8cd71b2d67c954e06a3a13ff5c2ca2cac914a
Jamil Palacios <japalac5@outlook.com>
2023-07-26 09:28:29 -0400

    movement error resolved

--- Sheep2/TS4.py
+++ Sheep2/TS4.py
@@ -18,32 +18,34 @@ def move_player():
   elif key == 'g':
     u_x += 1
     u_y += 1
-  if key == 'b':
+  elif key == 'b':
     u_x += 0
     u_y += 1
-  if key == 'v':
+  elif key == 'v':
     u_x -= 1
     u_y += 1
-  if key == 'c':
+  elif key == 'c':
     u_x -= 1
     u_y += 0
-  if key == 'd':
+  elif key == 'd':
     u_x -= 1
     u_y -= 1
-  if key == 'e':
+  elif key == 'e':
     u_x -= 0
     u_y -= 1
-  if key == 'r':
+  elif key == 'r':
     u_x += 1
     u_y += 1
-  move_to(u_shape, (Circle((10*u_x+5,10*u_y+5),10, filled=True, color="#00FF00")))
+  move_to(u_shape, ((10*u_x+5,10*u_y+5)))
 
 def pas():
   time.sleep(5)
 
 begin_graphics()  
+finished = False
 place_player()
-move_player()
+while not finished:
+  move_player()
 #key_text = Text("a", (320, 240), size=48)
 
 end_graphics()
