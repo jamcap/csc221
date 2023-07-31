5e836e335c7a9d03590ed180506e808d45f9a2f9
Jamil Palacios <japalac5@outlook.com>
2023-07-31 11:30:35 -0400

    scope of the project has changed, new focus on working with provided example

--- Sheep2/TS4.py
+++ Sheep2/TS4.py
@@ -2,65 +2,132 @@ from gasp import *
 from random import randint
 import time
 
+class User():
+  pass
 
-def check_collision():
-  global r_x, r_y, u_x, u_y, finished, done
-  if r_x == u_x and r_y == u_y: finished= True; done = Text("game over", (320, 240), size=48) 
-  pas()
+class Rob():
+  pass 
 
+def place_user():
+  global u
+  u = User()
+  u.x = randint(0, 63)
+  u.y = randint(0, 47)
 
-def place_player():
-  global u_x, u_y, u_shape
-  u_x = randint(0, 63)
-  u_y = randint(0, 47)
-  u_shape = (Circle((10*u_x+5,10*u_y+5),10, filled=True, color="#00FF00"))
+def no_danger_zone():
+  global u
+  place_user()
+  u.shape =Circle((10*u.x+5, 10*u.y+5), 8, filled=True, color="#008000")
 
 def place_rob():
-  global r_shape, r_y, r_x
-  r_x = randint(0, 63)
-  r_y = randint(0, 47)
-  r_shape = (Circle((10*r_x+5,10*r_y+5),10, filled=True, color="#800080"))
+  global robs
+  robs = []
+
+  while len(robs) > 8:
+    r = Rob()
+    r.x = (0,63)
+    r.y = (0, 47)
+    r.mothball = False
+    r.shape = Box((10*r.x, 10*r.y),10, 10, filled=True, color="#FFA500")
+    robs.append(r)
+  
+def move_player():
+  global User
+
+  key = update_when("key_pressed")
+
+  if key == 'r':
+    remove_from_screen(u.shape)
+    no_danger_zone()
+  elif key == 'w':
+    u.x += 0
+    u.y += 1
+  elif key == 'd':
+    u.x += 1
+    u.y += 0
+  elif key == 's':
+    u.x += 0
+    u.y -= 1
+  elif key == 'a':
+    u.x -= 1 
+    u.y += 0
+  move_to(u.shape, Circle((10*u.x+5, 10*u.y+5), 8, filled=True, thickness=8, color="#008000"))
 
 def move_rob():
-  global r_shape, r_x, r_y
-  if u_x > r_x: 
-    r_x += 1
-  elif u_y > r_y:
-    r_y += 1
-  elif u_y < r_y: 
-    r_y -= 1
-  elif u_x < r_x:
-    r_x -= 1
-  move_to(r_shape, ((10*r_x+5,10*r_y+5)))
+  global robs
+  for r in robs:
+    if not r.mothball:
+      if r.x > u.x:
+        r.x -= 1
+      elif r.y > u.y:
+        r.y -1
+      
+      if r.x < u.x:
+        r.x += 1
+      elif r.y < u.y:
+        r.y += 1
+    move_to(r.shape,(10*r.x, 10*r.y), 10, 10, filled=True, color="#FFA500")
 
-def move_player():
-  global u_x, u_y, u_shape
- 
-  key = update_when('key_pressed')
-  if key == 'w':
-   u_x += 0
-   u_y += 1
-  elif key == 'a':
-    u_x -=1
-    u_y +=0
-  elif key == 's':
-    u_x += 0
-    u_y -=1
-  elif key == 'd':
-    u_x += 1
-    u_y +=0
-  move_to(u_shape, ((10*u_x+5,10*u_y+5)))
+def collision_detected(item1, list):
+  for item2 in list:
+    if item1.x == item2.x and item1.y == item2.y: 
+      return True
+    return False
+
+def mothball(bot):
+  for bot1 in robs:
+    if bot1 == bot:
+      return False
+    if bot1.x == bot.x and bot1.y == bot.y:
+      return bot1
+    return False
+  
+def check_collisions():
+  global finished, robs
+
+  if collision_detected(User, Rob):
+    finished = True
+    Text("game over", 320, 340, size =22)
+    pas()
+    return
+  
+  survived_rob = []
+
+  for r in robs: 
+    if collision_detected(r, waste): 
+      continue
+
+  husk = mothball(r)
+  if not husk:
+    survived_rob.append(r)
+  else:
+    remove_from_screen(husk.shape)
+    husk.waste(True)
+    husk.shape = Box((10*r.x, 10*r.y), 10, 10, filled=True, color="#FFA500")
+    waste.append(husk)
+
+  robs = []
+  for r in survived_rob:
+    if not collision_detected(r, husk):
+      robs.append(r)
+
+  if not robs:
+    finished = True
+    Text("you win", (160, 240), size = 22)
+    pas()
+    return
 
 def pas():
   time.sleep(5)
 
 begin_graphics()  
 finished = False
-place_player()
+waste = []
 place_rob()
+no_danger_zone()
 while not finished:
     move_player()
     move_rob()
-    check_collision()
+    check_collisions()
  
 end_graphics()
